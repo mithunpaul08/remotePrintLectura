b581bdbd44a2de26d119c1b5574781c961ee0e07
mithun <mithunpaul08@gmail.com>
2019-04-24 16:18:25 -0700

    will take printer name also from command line

--- printLectura_ssh.sh
+++ printLectura_ssh.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/expect -f
 set filename [lindex $argv 0];
+set printer [lindex $argv 1];
 spawn scp "$filename" mithunpaul@lectura.cs.arizona.edu:./
 interact
 spawn ssh mithunpaul@lectura.cs.arizona.edu
 expect "$ "
-send "lpr -P lw0 $filename\r"
+send "lpr -P $printer $filename\r"
 exit
 interact
